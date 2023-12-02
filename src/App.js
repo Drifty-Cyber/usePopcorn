dd71eec190cf08a3f97a0f3de94a766a99e271c8
Drifty-Cyber <raphaelfadimu@gmail.com>
2023-12-02 17:03:48 +0100

    chore: add watched movies to local storage

--- src/App.js
+++ src/App.js
@@ -56,10 +56,14 @@ const KEY = "7f212130";
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [selectedId, setSelectedId] = useState(null);
+  // const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
 
   function handleSelectedMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -71,11 +75,20 @@ export default function App() {
 
   function handleAddWatched(movie) {
     setWatched((watchedMovies) => [...watchedMovies, movie]);
+
+    // localStorage.setItem("watched", JSON.stringify([...watched, movie]));
   }
 
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
